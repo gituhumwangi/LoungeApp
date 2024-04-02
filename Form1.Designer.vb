08e9a8e6b00f30f0d6a79bc04184ef84aa36ff65
QMaru-1 <148209129+QMaru-1@users.noreply.github.com>
2024-04-02 19:40:46 +0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Billing
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CalculateBill = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CalculateBill
+        '
+        Me.CalculateBill.Location = New System.Drawing.Point(118, 41)
+        Me.CalculateBill.Name = "CalculateBill"
+        Me.CalculateBill.Size = New System.Drawing.Size(198, 59)
+        Me.CalculateBill.TabIndex = 0
+        Me.CalculateBill.Text = "CalculateBill"
+        Me.CalculateBill.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(121, 143)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(196, 66)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "ApplyDiscount"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(125, 249)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(197, 65)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "PrintInvoice"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Billing
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CalculateBill)
+        Me.Name = "Billing"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CalculateBill As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Billing
+    Private Sub btnCalculateBill_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Calculate total bill based on stay duration and services used
+        Dim stayDuration As Integer = Convert.ToInt32(txtStayDuration.Text)
+        Dim additionalServicesCost As Double = 0 ' Calculate cost of additional services here
+
+        Dim totalBill As Double = (stayDuration * nightlyRate) + additionalServicesCost
+        Dim discount As Double = 0
+
+        ' Apply discount if any
+        totalBill -= discount
+
+        ' Display total bill in a label or textbox
+        lblTotalBill.Text = "Total Bill: $" & totalBill.ToString()
+    End Sub
+
+    Private Sub btnPrintInvoice_Click(sender As Object, e As EventArgs) Handles btnPrintInvoice.Click
+        ' Code to print the invoice goes here
+    End Sub
+End Class
